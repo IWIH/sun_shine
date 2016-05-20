75e6e520fa6b5bb0e3fc9a7b084509082187dccb
iwih <osama.a.shammari@gmail.com>
2016-05-20 19:15:14 +0300

    fixing fragments issues after understanding what fragments are @ https://app.pluralsight.com/library/courses/android-fragments/table-of-contents

---
+++ app/src/main/java/com/wordpress/iwih/sunshine/list_item_forecast.java
@@ -0,0 +1,29 @@
+package com.wordpress.iwih.sunshine;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class list_item_forecast extends Fragment {
+
+
+    public list_item_forecast() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_item_forecast, container, false);
+    }
+
+}
