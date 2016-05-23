189ab66fe6a8888381a873f358bcda96415f302a
Osama Alwash <osama.a.shammari@gmail.com>
2016-05-23 23:51:00 +0300

    Connection to cloud

--- app/src/main/java/com/wordpress/iwih/sunshine/ForecastFragment.java
+++ app/src/main/java/com/wordpress/iwih/sunshine/ForecastFragment.java
@@ -5,12 +5,15 @@ import android.app.Fragment;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,9 +21,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -28,7 +35,8 @@ import java.util.List;
  */
 public class ForecastFragment extends Fragment {
 
-    final Logger log = new Logger("ForecastFragment");
+    final Logger log = new Logger(this.getClass().getSimpleName());
+    public ArrayAdapter<String> forecastArrayAdapter;
 
     public ForecastFragment() {
         // Required empty public constructor
@@ -38,30 +46,21 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        // Inflate the layout for this fragment
         View inflate = inflater.inflate(R.layout.forecast_fragment, container, false);
-        Log.v("forecast_fragment", "Fragment Inflated!");
-        String[] weekForecastArray = {
-                "Saturday",
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday"};
+        log.v("Fragment Inflated!");
 
-        List<String> forecastList =
-                new ArrayList<>(Arrays.asList(weekForecastArray));
-
-        ArrayAdapter<String> forecastAdapter =
-                new ArrayAdapter<>(
-                        getActivity(),
-                        R.layout.forecast_list_item_fragment,
-                        R.id.forecast_text_view,
-                        forecastList);
+        forecastArrayAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.forecast_list_item_fragment,
+                R.id.forecast_text_view,
+                Arrays.asList(new String[]{""}));
 
         ListView forecastListView = (ListView) inflate.findViewById(R.id.forecast_list_view);
-        forecastListView.setAdapter(forecastAdapter);
+        forecastListView.setAdapter(forecastArrayAdapter);
 
-        // Inflate the layout for this fragment
+        fetchWeatherData();
+
         return inflate;
 
     }
@@ -71,7 +70,7 @@ public class ForecastFragment extends Fragment {
         new FetchWeatherAsync().execute("As Samawah", "json", "metric", "7");
     }
 
-    private class FetchWeatherAsync extends AsyncTask<String, Void, Void> {
+    public class FetchWeatherAsync extends AsyncTask<String, Void, Void> {
 
         private String cityName;
         private String dataMode;
@@ -91,10 +90,16 @@ public class ForecastFragment extends Fragment {
             log.v("Data passed to internal variables successfully!");
 
             fetchWeatherJsonData();
+
+            log.v("Start parsing weather data...");
+            parseForecastJsonStr();
+
             return null;
         }
 
-        public String fetchWeatherJsonData() {
+        private String forecastJsonStr;
+
+        public void fetchWeatherJsonData() {
 
             HttpURLConnection urlConnection = null;
 
@@ -129,7 +134,7 @@ public class ForecastFragment extends Fragment {
                 }
             }
 
-            return forecastJsonStr;
+            this.forecastJsonStr = forecastJsonStr;
         }
 
         private String getForecastJsonStringFromUrlConnection
@@ -181,6 +186,149 @@ public class ForecastFragment extends Fragment {
                     .appendQueryParameter("appid", getString(R.string.owm_appid));
 
             return uriBuilder.build().toString();
+        }
+
+        private void parseForecastJsonStr() {
+            try {
+                JSONObject mainJson = new JSONObject(forecastJsonStr);
+
+                parseCityAndCountryName(mainJson.getJSONObject("city"));
+
+                parseWeatherData(mainJson.getJSONArray("list"));
+            } catch (JSONException e) {
+                log.e("Couldn't parse forecastJsonStr or/and display its content: " + e.getMessage());
+            }
+
+        }
+
+        private String activityTitle;
+
+        private void parseCityAndCountryName(JSONObject cityJsonObj) {
+            try {
+                String cityName = cityJsonObj.getString("name");
+                String countryName = cityJsonObj.getString("country");
+
+                String activityTitle = cityName + ", " + countryName;
+                log.i("City name title: " + activityTitle);
+
+                this.activityTitle = activityTitle;
+            } catch (JSONException e) {
+                log.e("Couldn't parse city/country name(s): " + e.getMessage());
+            }
+        }
+
+        private ArrayList<String> forecastAListStr;
+
+        private void parseWeatherData(JSONArray weatherDataJsonArray) {
+
+            ArrayList<String> forecastAListStr = new ArrayList<>();
+            try {
+                for (int i = 0; i < weatherDataJsonArray.length(); i++) {
+
+                    Date weatherDate = getWeatherDayDate(weatherDataJsonArray.getJSONObject(i));
+
+                    JSONObject dayTempJsonObj = weatherDataJsonArray.getJSONObject(i).getJSONObject("temp");
+                    Long maxTemp = getDayMaxTemp(dayTempJsonObj);
+                    Long minTemp = getDayMinTemp(dayTempJsonObj);
+                    Long dayTemp = getDayTemp(dayTempJsonObj);
+
+                    DateFormat dateFormat = new SimpleDateFormat("EEEE d/M");
+                    String dayForecastRow = (new StringBuilder())
+                            .append(dateFormat.format(weatherDate))
+                            .append(" - ")
+                            .append("Day: ")
+                            .append(dayTemp)
+                            .append(", ")
+                            .append("Max: ")
+                            .append(maxTemp)
+                            .append(", ")
+                            .append("Min: ")
+                            .append(minTemp).toString();
+
+                    log.i("Finished parsing day weather: " + dayForecastRow);
+
+                    forecastAListStr.add(dayForecastRow);
+
+                }
+
+            } catch (JSONException e) {
+                log.e("Couldn't parse weather data: " + e.getMessage());
+            }
+
+            this.forecastAListStr = forecastAListStr;
+        }
+
+        private Date getWeatherDayDate(JSONObject dayWeatherObj) {
+            Date weatherDate = null;
+            try {
+
+                weatherDate = new Date(dayWeatherObj.getLong("dt") * 1000);
+
+            } catch (JSONException e) {
+                log.e("Couldn't parse weather date: " + e.getMessage());
+            }
+
+            return weatherDate;
+        }
+
+        private Long getDayMaxTemp(JSONObject dayTempJsonObj) {
+            Long dayMaxTemp = null;
+            try {
+                dayMaxTemp = dayTempJsonObj.getLong("max");
+            } catch (JSONException e) {
+                log.e("Couldn't parse maximum temp of the day: " + e.getMessage());
+            }
+
+            return dayMaxTemp;
+        }
+
+        private Long getDayMinTemp(JSONObject dayTempJsonObj) {
+            Long dayMinTemp = null;
+            try {
+                dayMinTemp = dayTempJsonObj.getLong("min");
+            } catch (JSONException e) {
+                log.e("Couldn't parse minimum temp of the day: " + e.getMessage());
+            }
+
+            return dayMinTemp;
+        }
+
+        private Long getDayTemp(JSONObject dayTempJsonObj) {
+            Long dayTemp = null;
+            try {
+                dayTemp = dayTempJsonObj.getLong("day");
+            } catch (JSONException e) {
+                log.e("Couldn't parse mid-day temp of the day: " + e.getMessage());
+            }
+
+            return dayTemp;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            if (forecastJsonStr.equals("") || (forecastJsonStr == null))
+                return;
+            log.v("Start populating weather data to UI...");
+
+            setActivityTitleToCityName();
+
+            populateWeatherArrayToUI();
+        }
+
+        private void populateWeatherArrayToUI() {
+            forecastArrayAdapter.clear();
+            for (String dayWeather : forecastAListStr) {
+                forecastArrayAdapter.add(dayWeather);
+            }
+        }
+
+        private void setActivityTitleToCityName() {
+            try {
+                getActivity().setTitle(activityTitle);
+            } catch (Exception e) {
+                log.e("Couldn't change activity title to city name: " + e.getMessage());
+            }
         }
     }
 
--- app/src/main/java/com/wordpress/iwih/sunshine/MainActivity.java
+++ app/src/main/java/com/wordpress/iwih/sunshine/MainActivity.java
@@ -3,10 +3,10 @@ package com.wordpress.iwih.sunshine;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-
-import java.io.IOException;
-
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +20,42 @@ public class MainActivity extends AppCompatActivity {
         logger.v("Activity Inflated");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId){
+            case R.id.menu_main_activity_settings:
+                Toast.makeText(
+                        MainActivity.this,
+                        "You called me from app menu!!",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            case R.id.menu_main_activity_refresh:
+                refreshWeatherData();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void refreshData(View view)  {
+        refreshWeatherData();
+    }
+
+    private void refreshWeatherData() {
         FragmentManager fragManager = getFragmentManager();
         ForecastFragment forecastFragment = (ForecastFragment)
                 fragManager.findFragmentById(R.id.forecast_fragment_main);
         forecastFragment.fetchWeatherData();
     }
+
 
 }
 
